de33d3b54ce0a37363e582f19bb1dd980b6b4088
robertgrc <robert.rodriguez.rgrc@gmail.com>
2024-01-12 16:45:52 -0400

    Modificaciones en routes.ts

---
+++ src/routes/routes.ts
@@ -0,0 +1,29 @@
+
+import { LazyPage1, LazyPage2, LazyPage3 } from '../01-lazyload/pages';
+
+// type JSXComponent = () => JSX.Element;
+
+interface Route{
+    path: string;
+    component:()=>JSX.Element;
+    name: string;
+    children?:Route[]
+}
+
+export const routes:Route[] =[
+    {
+        path: '/lazy1',
+        component: LazyPage1,
+        name: 'LazyPage-1'
+    },
+    {
+        path: '/lazy2',
+        component: LazyPage2,
+        name: 'LazyPage-2'
+    },
+    {
+        path: '/lazy3',
+        component: LazyPage3,
+        name: 'LazyPage-3'
+    },
+]
