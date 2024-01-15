8f9e8094ae94ff45b62ae5a42b64dc82ab1ed4cf
robertgrc <robert.rodriguez.rgrc@gmail.com>
2024-01-15 10:08:37 -0400

    Lazy load Nested

---
+++ src/01-lazyload/layout/LazyLayout.tsx
@@ -0,0 +1,11 @@
+import { Navigation } from "../router/Navigation";
+
+export const LazyLayout = () => {
+  return (
+    <>
+     <h1>LazyLayout - Main Ppage</h1>
+     <Navigation />
+    </>
+  )
+}
+export default LazyLayout;
--- src/01-lazyload/pages/index.ts
+++ src/01-lazyload/pages/index.ts
@@ -1,3 +1,5 @@
+export { NoLazy } from './NoLazy';
 export { LazyPage1 } from './LazyPage1';
 export { LazyPage2 } from './LazyPage2';
 export { LazyPage3 } from './LazyPage3';
+
